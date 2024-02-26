ad86ebe090d29c6c075b377ab2ff3c01e0e8d9c0
amanecopse <amanecopse@gmail.com>
2024-02-26 10:25:29 +0900

    feat: 로그인 인증 오류 핸들링
    500으로 처리되는 로그인 오류들을 아이디 및 비밀번호 불일치 오류로서 핸들링하고 401코드로써 반환한다

--- src/main/java/com/gg/mafia/domain/member/application/AuthService.java
+++ src/main/java/com/gg/mafia/domain/member/application/AuthService.java
@@ -1,11 +1,14 @@
 package com.gg.mafia.domain.member.application;
 
+import com.gg.mafia.domain.member.exception.LoginFailedException;
 import lombok.RequiredArgsConstructor;
 import com.gg.mafia.domain.member.dao.UserDao;
 import com.gg.mafia.domain.member.dto.LoginRequest;
 import com.gg.mafia.domain.member.dto.SignupRequest;
 import com.gg.mafia.domain.member.dto.UserMapper;
 import com.gg.mafia.global.config.security.TokenProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -31,7 +34,14 @@ public class AuthService {
     public String authenticate(LoginRequest request) {
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        Authentication authentication;
+        try {
+            authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        } catch (BadCredentialsException e) {
+            throw new LoginFailedException("비밀번호가 일치하지 않습니다");
+        } catch (InternalAuthenticationServiceException e) {
+            throw new LoginFailedException("존재하지 않는 유저입니다");
+        }
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return tokenProvider.createToken(authentication);
     }
