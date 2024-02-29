934aa6613c7f995a2a9d64a6d4e89d4eba6e34ec
taewoo <dlscksdlsi2@gmail.com>
2024-03-01 00:24:52 +0900

    feat: profileEntity작성 , 리뷰필요

---
+++ src/main/java/com/gg/mafia/domain/Profile/domain/ProfileEntity.java
@@ -0,0 +1,32 @@
+package com.gg.mafia.domain.Profile.domain;
+
+import com.gg.mafia.domain.member.domain.User;
+import com.gg.mafia.domain.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ProfileEntity extends BaseEntity {
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private int rating;
+    @ManyToOne
+    //profile의 외래키열의 이름은 user_id 참조하는 열은 id라 생각해서 작성
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @Column(nullable = false)
+    private User user;
+}
