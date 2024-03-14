c135db35f8c6f472fc8ee279d42dabaa9d37ac6d
wooyong99 <wulovesk@naver.com>
2024-03-14 16:38:46 +0900

    feat : 로그 추적기

--- src/main/java/com/gg/mafia/global/config/AppConfig.java
+++ src/main/java/com/gg/mafia/global/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.gg.mafia.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -13,6 +14,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
         @PropertySource("classpath:config/swagger.properties"),
         @PropertySource("classpath:config/security.properties")
 })
+@EnableAspectJAutoProxy
 public class AppConfig {
     @Bean
     public PropertySourcesPlaceholderConfigurer dataSource() {
