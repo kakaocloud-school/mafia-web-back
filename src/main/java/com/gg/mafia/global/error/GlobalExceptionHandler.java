305c9df8ccfa1fd503cbecc6e1bc3edc13da0348
amanecopse <amanecopse@gmail.com>
2024-03-20 16:06:46 +0900

    feat: 회원가입 에러 핸들링 추가

--- src/main/java/com/gg/mafia/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/gg/mafia/global/error/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.gg.mafia.global.error;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.gg.mafia.domain.member.exception.LoginFailedException;
+import com.gg.mafia.domain.member.exception.SignupFailedException;
 import com.gg.mafia.global.common.response.ApiResponse;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.stream.Collectors;
@@ -48,7 +49,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .body(ApiResponse.error(message));
     }
 
-    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class, SignupFailedException.class})
     ResponseEntity<ApiResponse<Void>> handleBadRequestException(RuntimeException exception) {
         logger.error("message", exception);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
