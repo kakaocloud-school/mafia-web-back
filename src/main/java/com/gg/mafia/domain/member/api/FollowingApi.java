77415e40820dc00309281adf1e1d7add36670685
wooyong99 <wulovesk@naver.com>
2024-03-27 15:30:45 +0900

    core : develop 브랜치 병합

---
+++ src/main/java/com/gg/mafia/domain/member/api/FollowingApi.java
@@ -0,0 +1,12 @@
+package com.gg.mafia.domain.member.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class FollowingApi {
+    private final FollowingSer
+}
