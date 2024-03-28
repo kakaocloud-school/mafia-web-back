768a8217b38b99451db3596aa8830d7f855d840c
wooyong99 <wulovesk@naver.com>
2024-03-28 20:06:23 +0900

    feat : Comment Service 클래스 생성

---
+++ src/main/java/com/gg/mafia/domain/comment/application/CommentService.java
@@ -0,0 +1,10 @@
+package com.gg.mafia.domain.comment.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+}
